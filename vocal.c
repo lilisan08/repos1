674c18ce6091f6f37629462581e13d34ea71bd2c
lilis <168390254+lilisan08@users.noreply.github.com>
2024-04-29 21:07:17 -0600

    Add files via upload

---
+++ vocal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main ()
+{
+	char l;
+	printf("ingresa una letra:");
+	scanf("%c", &l);
+	if(l=='a' || l=='A'){
+ printf("La letra ingresada es una vocal: %c\n",l);
+ }
+ else if(l=='e' || l=='E'){
+ printf("La letra ingresada es una vocal: %c\n",l);
+ 
+}
+ else if(l=='i' || l=='I'){
+ printf("La letra ingresada es una vocal: %c\n",l);
+ 
+ }
+ else if(l=='o' || l=='O'){
+ printf("La letra ingresada es una vocal: %c\n",l);
+ 
+ }
+ else if(l=='u' || l=='U'){
+ printf("La letra ingresada es una vocal: %c\n",l);
+ 
+ }
+ return 0;
+}
