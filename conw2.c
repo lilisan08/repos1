b76d86a565377aa4a0c51418d51c51c5b5a979fa
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-02 16:39:00 -0600

    Add files via upload

---
+++ conw2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 1;
+
+    while(numero <= 100) {
+        printf("%d\n", numero);
+        numero += 2;
+    }
+
+    return 0;
+}
---
+++ conw3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 100;
+
+    while(numero >= 1) {
+        printf("%d\n", numero);
+        numero--;
+    }
+
+    return 0;
+}
