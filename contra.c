5d9c7271b329bb14b2c5d6637662713999703207
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-04 17:16:29 -0600

    Add files via upload

---
+++ contra.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+	char n[15];
+	int contra, i;
+	printf("ingresa tu nombre:");
+    scanf("%c", &n[15]);
+   if (n, "root")
+    {
+     for (i=1; i <=3; i++) 
+     {
+     printf("Ingresa un pin:");
+     scanf("%d",&contra);
+     if (contra == 1234)
+     {
+      printf("EL pin es correcto\n");
+      break;
+     }else if (contra != 1234) {
+       printf("El pin es incorrecto");
+      } 
+      else if (n != "root") 
+     {
+      printf("El nombre es incorrecto");
+     }
+
+ return 0;
+}
