89b93a6bf2923d396fb60169afec72ea6f17959b
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-04 15:09:43 -0600

    Add files via upload

--- nume.c
+++ nume.c
@@ -4,19 +4,19 @@ int main() {
     int num, sum = 0, cont = 0;
     
     do {
-        printf("Introduce un número 0:para finalizar:");
+        printf("Introduce un numero 0:para finalizar:");
         scanf("%d", &num);
         
         if(num != 0) {
-            printf("Has introducido el número %d", num);
+            printf("Has introducido el numero: %d\n", num);
             sum += num;
             cont++;
         }
-    } while(num = 0);
+    } while(num != 0);
     
-    printf("Se ha introducido el número 0.\n");
-    printf("La cantidad de números es de: %d\n", cont);
-    printf("La suma de los números: %d\n", sum);
+    printf("Se ha introducido el numero 0\n");
+    printf("La cantidad de numeros es de: %d\n", cont);
+    printf("La suma de los numeros es: %d\n", sum);
     
     return 0;
 }
