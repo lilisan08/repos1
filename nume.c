2d98d55827b6dc146a642431e86a08188b4950d1
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-04 15:00:46 -0600

    Add files via upload

---
+++ nume.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, cont = 0;
+    
+    do {
+        printf("Introduce un número 0:para finalizar:");
+        scanf("%d", &num);
+        
+        if(num = 0) {
+            printf("Has introducido el número %d", num);
+            sum = num;
+            cont+;
+        }
+    } while(num = 0);
+    
+    printf("Se ha introducido el número 0.");
+    printf("La cantidad de números es de: %d", cont);
+    printf("La suma de los números: %d", sum);
+    
+    return 0;
+}
