1e2893d1febff233296f49e864a71396d3ffc10a
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-06 16:48:52 -0600

    Add files via upload

--- 10nums.c
+++ 10nums.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int numeros;
+    int numeros[10];
     int i;
     int mayor, menor, suma;
     printf("Ingresa 10 numeros positivos:\n");
     for (i = 0; i < 10; i++) {
-        printf("Numero :%d ", i );
-        scanf("%d", &numeros);
-        while (numeros <= 0) {
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+        while (numeros[i] <= 0) {
             printf("Ingresa un numero positivo: ");
-            scanf("%d", &numeros);
+            scanf("%d", &numeros[i]);
         }
         if (i == 0) {
-            mayor = menor = numeros;
-            suma = numeros;
+            mayor = menor = numeros[i];
+            suma = numeros[i];
         } else {
-            if (numeros > mayor) {
-                mayor = numeros;
+            if (numeros[i] > mayor) {
+                mayor = numeros[i];
             }
-            if (numeros < menor) {
-                menor = numeros;
+            if (numeros[i] < menor) {
+                menor = numeros[i];
             }
-            suma = suma + numeros;
+            suma += numeros[i];
         }
     }
     printf("El mayor numero es: %d\n", mayor);
