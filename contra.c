2945531981e33277a8d55e1fdbbb0ac03ac9306e
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-13 12:26:54 -0600

    Add files via upload

--- contra.c
+++ contra.c
@@ -1,9 +1,12 @@
-#include <stdio.h>//vercion 2 examen practico 13/05/2024
+#include <stdio.h>//vercion 3 y 4 examen practico 13/05/2024 (me confundi y no suvi la version 3)
 #include <string.h>
-int main() 
-{
+#include <string.h>
+int main() {
   char n[15], contra[15];
-  int intentos =3;
+  int intentos =3, preci;
+  float total = 0, cantidad;
+   float precioUnitario;
+
     do{
    printf("ingresa tu nombre de usuario:");
     scanf("%s", &n);
@@ -12,15 +15,26 @@ int main()
    if (strcmp(n, "lilian") == 0 && strcmp(contra, "2bm38") == 0){
     printf("Bienvenido al sisitema:%s\n",n);
       break;
-     }else{
-        intentos--;
-       if (intentos>0){
-       printf("alguno de los datos es incorrecto; quedan %d intentos.n", intentos); 
-     }else {
-      printf("Se han acabado los intentos permitidos.\n");
-      break;
-     }
-    }
-  }while(intentos>0);
- return 0;
+        } else {
+            intentos--;
+            if (intentos > 0) {
+                printf("Nombre de usuario o contrasea incorrectos. Te quedan %d intentos.\n", intentos);
+        }
+
+   } while (cantidad != 0);
+    printf ("para terminar introduce la cantidad de 0 \n\n");
+      printf("Muy bien, Ingresa la cantidad de tus articulos vendidos  ");
+        scanf  ("%d", &cantidad);
+        printf ("de que precio fueron? ");
+        scanf ("%d", &preci);
+        if (preci < 0||cantidad < 0 ){
+            printf ("lo siento, tus cantidades son negativas, Ingresa otra cantidad .\n\n");
+           } else {
+                float preci=precioUnitario*cantidad;
+                total += preci;
+            }
+       printf ("tu total de los productos es de %d pesos ", total); 
+   }
+   while(cantidad<0);
+    return 0;
 }
