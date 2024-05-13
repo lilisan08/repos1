733795ed4f383f700201377a623f65d5b0d5676c
lilis <168390254+lilisan08@users.noreply.github.com>
2024-05-13 16:14:37 -0600

    Add files via upload

--- contra.c
+++ contra.c
@@ -1,40 +1,44 @@
 #include <stdio.h>//vercion 3 y 4 examen practico 13/05/2024 (me confundi y no suvi la version 3)
 #include <string.h>
-#include <string.h>
 int main() {
-  char n[15], contra[15];
-  int intentos =3, preci;
-  float total = 0, cantidad;
-   float precioUnitario;
+  char n[20];
+    char contra[20];
+    int precioUnitario, cantidad;
+    int intentos = 3;
+    int total,preci;
 
-    do{
-   printf("ingresa tu nombre de usuario:");
-    scanf("%s", &n);
-    printf("ingresa la contrasena");
-    scanf("%s", &contra);
-   if (strcmp(n, "lilian") == 0 && strcmp(contra, "2bm38") == 0){
-    printf("Bienvenido al sisitema:%s\n",n);
-      break;
+    do {
+        printf("Introduce tu nombre: ");
+        scanf("%s", &n);
+
+        printf("Introduce tu contraseña: ");
+        scanf("%s", &contra);
+
+        if (strcmp(n, "lilian") == 0 && strcmp(contra, "2b38") == 0) {
+            printf("Bienvenido al sistema.\n"); 
+            break;
         } else {
             intentos--;
             if (intentos > 0) {
                 printf("Nombre de usuario o contrasea incorrectos. Te quedan %d intentos.\n", intentos);
+            } else {
+                printf("Se ha superado el número de intentos permitido.\n");
+                break;
+            }
         }
-
-   } while (cantidad != 0);
+    } while (cantidad != 0);
     printf ("para terminar introduce la cantidad de 0 \n\n");
-      printf("Muy bien, Ingresa la cantidad de tus articulos vendidos  ");
+      printf("Ingresa la cantidad de los articulos");
         scanf  ("%d", &cantidad);
-        printf ("de que precio fueron? ");
-        scanf ("%d", &preci);
-        if (preci < 0||cantidad < 0 ){
+        printf ("ingresa el precio unitario del permitido:");
+        scanf ("%d", &precioUnitario);
+        if (cantidad < 0||preci < 0 ){
             printf ("lo siento, tus cantidades son negativas, Ingresa otra cantidad .\n\n");
            } else {
-                float preci=precioUnitario*cantidad;
-                total += preci;
+                float preci=cantidad*precioUnitario;
+                total +=preci;
             }
        printf ("tu total de los productos es de %d pesos ", total); 
-   }
-   while(cantidad<0);
+
     return 0;
 }
